e2d81219b128e5b0a60b8a7cfbf41cd8085b5345
bzung <bzung231@gmail.com>
2018-11-04 14:57:03 +0545

    changed the title to SR HOTEL

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'The SR Hotel ,luxury Hotel in Butwal- Bhairahawa, Nepal') }}</title>
+    <title>{{  'The SR Hotel ,luxury Hotel in Butwal- Bhairahawa, Nepal' }}</title>
 
     <!-- Scripts -->
 {{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
