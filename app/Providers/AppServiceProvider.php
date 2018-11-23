c3c2e47f4ee898aefbe9142f2bf49ef350fdf0d1
Prakash Chhetri <sohanmax02@gmail.com>
2018-11-23 15:57:16 +0545

    resolved issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,8 +30,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->bind('path.public', function() {
-            return base_path('public_html');
-        });
+
     }
 }
