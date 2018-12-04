ef2a11d64add2533956eb9b5339543c05aa0f892
bzung <bzung231@gmail.com>
2018-12-04 19:59:22 +0545

    subject updated to mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,7 +11,7 @@ class ContactController extends Controller
 
 	public function send()
 	{
-		Mail::to('bzung231@gmail.com')->send(new Contact());
+		Mail::to('thesrhotel@gmail.com')->send(new Contact());
 		return redirect()
 			->route('contact')
 			->with(['message' => "Your email has been successfully sent "]);
