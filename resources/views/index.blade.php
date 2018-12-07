9352d9dcc312477ae7c58003b726abc442a64e05
bzung <bzung231@gmail.com>
2018-12-07 13:39:50 +0545

    guest types added in a reservation bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,18 +55,18 @@
     <div class="reservation-bar">
         <div class="container">
             <div class="row">
-                <form action="#">
+                <form action="#" method="post">
                     <div class="col-md-6 col-sm-12">
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="input-append date" id="dpd1" data-date="Check In" data-date-format="dd-mm-yyyy">
-                                    <input class="form-control" size="16" type="text" value="Arrive date">
+                                    <input class="form-control" name="arrive_date" size="16" type="text" value="Arrive date">
                                     <span class="icon-calendar"></span>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="input-append date" id="dpd2" data-date="Check Out" data-date-format="dd-mm-yyyy">
-                                    <input class="form-control" size="16" type="text" value="Departure date">
+                                    <input class="form-control" name="departure_date" size="16" type="text" value="Departure date">
                                     <span class="icon-calendar"></span>
                                 </div>
                             </div>
@@ -77,18 +77,52 @@
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <div class="fake-select">
-                                        <select>
-                                            <option value="Adult" selected>Adult</option>
-                                            <option>Children</option>
-                                        </select>
+
+                                        <input class="form-control" name="guest_numbers" data-toggle="collapse" type="text" data-target="#guest-modal" value="2 Adults 1 Children" readonly style="background-color:#353535">
+                                        <div class="collapse guest-modal" id="guest-modal">
+                                            <form>
+                                                <div class="form-group row">
+                                                    <label for="Children" class="col-sm-2 col-form-label">Children</label>
+                                                    <div class="col-sm-5 guest-modal-select">
+                                                        <select name="children" id="child-num">
+                                                            <option value="0">0</option>
+                                                            <option value="1">1</option>
+                                                            <option value="2">2</option>
+                                                            <option value="3+">3+</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="adult" class="col-sm-2 col-form-label">Adult</label>
+                                                    <div class="col-sm-5 guest-modal-select">
+                                                        <select name="adult" id="adult-num">
+                                                            <option value="0">0</option>
+                                                            <option value="1">1</option>
+                                                            <option value="2">2</option>
+                                                            <option value="3+">3+</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </form>
+                                        </div>
+
                                     </div>
                                 </div>
+
+
                             </div>
+
+
                             <div class="col-sm-4">
                                 <div class="form-group">
                                     <div class="fake-select">
-                                        <select disabled>
-                                            <option value="Room" selected >Room</option>
+                                        <select name="room_type">
+                                            <option value="suite" selected>Suite Room</option>
+                                            <option value="deluxe" selected>Deluxe Room</option>
+                                            <option value="standard" selected>Standard Room</option>
+                                            <option value="honeymoon" selected>Honeymoon Suite Room</option>
+
                                         </select>
                                     </div>
                                 </div>
@@ -113,9 +147,9 @@
                             <h1 class="animate-title title">Warmest welcome for our guest</h1>
                             <p>Located in the foothills of chure hills and on the southern plains of Terai region of Nepal, The SR Hotel is committed and dedicated to providing you with the best hotel experience you can have while your stay in Nepal. With just 20 Kilometers North from Gautam Buddha Airport, Bhairahawa, and 40 Kilometers North from Lumbini, The SR Hotel is a perfect example of luxury and comfort.</p>
 
-                              <p> Equipped with 31 well furnished and spacious rooms wherein each room enjoys
-                               a beautiful view of the town. The Hotel has 9 Deluxe, 11 Luxury & 2 Honeymoon
-                               Suits spread out on Ground plus 4 Floor levels.
+                            <p> Equipped with 31 well furnished and spacious rooms wherein each room enjoys
+                                a beautiful view of the town. The Hotel has 9 Deluxe, 11 Luxury & 2 Honeymoon
+                                Suits spread out on Ground plus 4 Floor levels.
 
                             </p>
                             <a href="#" class="btn btn-default">Book Now</a>
@@ -368,4 +402,13 @@
 @endsection
 
 @push('footer-scripts')
+<script>
+    $(document).click(function (e) {
+        if (!$(e.target).is('.guest-modal ,.guest-modal *')) {
+            $('.collapse').collapse('hide');
+        }
+    });
+</script>
+
+
 @endpush
